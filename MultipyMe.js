893352a97f0da66d96af2f025fa4645a6cdcd08e
srac1777 <shashank.racherla@gmail.com>
2018-09-27 14:38:25 -0700

    finished first question

---
+++ MultipyMe.js
@@ -0,0 +1,23 @@
+// Write a multiplication function in javascript to use like this.
+// console.log(mul(2)(4)(5));
+
+const mul = (arg) => {
+    let result = 1;
+    let numArgs = 3;
+    let argValues = [];
+    const subMul = (el) => {
+        argValues.push(el);
+        if (argValues.length < numArgs){
+            result *= el;
+            return subMul;
+        } else {
+            result *= el;
+            return result;
+        }
+        
+    }
+    return subMul(arg);
+}
+
+console.log(mul(2)(4)(5));
+
