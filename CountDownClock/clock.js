4af28077036dc977c5e26c4885d1c0c95daadc23
srac1777 <shashank.racherla@gmail.com>
2018-09-27 16:32:34 -0700

    functionality done

---
+++ CountDownClock/clock.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+export default class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: '',
+            value: ''
+        };
+        this.updateTime = this.updateTime.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    updateTime() {
+        let timeLeft = this.state.time - 1;
+        this.setState({ time: timeLeft });
+    }
+
+    handleChange(e) {
+        e.preventDefault();
+        this.setState({ value: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.setState({ time: this.state.value * 60000 - 1}); //value * 60 because input is in minutes
+        if(this.intervalId) clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.updateTime, 1);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    }
+
+    render() {
+        let to_return = (<div>Loading...</div>)
+        if(this.state.time > 0){
+            let minutes = Math.floor(this.state.time / 60000);
+            let seconds = Math.floor(this.state.time / 1000) % 60;
+            let milliseconds = Math.floor(this.state.time) % 1000;
+
+            minutes = (minutes < 10) ? `0${minutes}` : minutes;
+            seconds = (seconds < 10) ? `0${seconds}` : seconds;
+            milliseconds = (milliseconds < 10) ? `0${milliseconds}` : milliseconds;
+
+            to_return = `${minutes}:${seconds}:${milliseconds}`;
+        } else if (this.state.time === '') {
+            to_return = (<div>Enter your Input above to get started!</div>);
+        } 
+        else if (this.state.time === 0){
+            to_return = (<div>Countdown Complete!</div>);
+        } else {
+            to_return = (<div>Invalid input</div>);
+        }
+
+        return (
+            <div>
+                <h1>Clock</h1>
+                <div>
+                    Enter the number of minutes you want to countdown:
+                    <input type="number" onChange={this.handleChange}/>
+                    <input type="submit" value="Start" onClick={this.handleSubmit}/>
+                    
+                </div>
+                <div>
+                    <span>
+                        Time:
+                    </span>
+                    <span>
+                        {/* {this.state.time} */}
+                        {to_return}
+
+                    </span>
+                </div>
+            </div>
+        );
+    }
+};
--- CountDownClock/index.jsx
+++ CountDownClock/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Clock from './clock';
 
 
 function Root() {
     return (
         <div>
-            hi
+            <Clock />
         </div>
     );
 }
